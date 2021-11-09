9613df7d77bb09efaa5e68f000e96c6fe9c24b6e
Sunstrike95 <82712376+Sunstrike95@users.noreply.github.com>
2021-11-09 12:51:40 +0200

    Add files via upload

---
+++ RPGgame/config.py
@@ -0,0 +1,39 @@
+WIN_WIDTH = 640 
+WIN_HEIGHT = 480
+FPS = 60
+
+TILESIZE = 32
+
+
+PLAYER_LAYER = 5
+ENEMY_LAYER = 4
+BLOCK_LAYER = 2
+
+GROUND_LAYER = 1
+
+PLAYER_SPEED = 3
+ENEMY_SPEED = 2
+
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+
+tilemap = [	
+	'BBBBBBBBBBBBBBBBBBBB',
+	'B..E...............B',
+	'B..................B',
+	'B....BBB...........B',
+	'B..................B',
+	'B........P.........B',
+	'B..................B',
+	'B..................B',
+	'B.....BBB..........B',
+	'B.......B..........B',
+	'B.......B.....E....B',
+	'B..................B',
+	'B..................B',
+	'B..................B',
+	'BBBBBBBBBBBBBBBBBBBB',
+]
+
